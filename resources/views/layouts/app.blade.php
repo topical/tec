d653897caedaa267fe9cf0579c7b0f6f74afbd70
Elea1234 <email@elea.cc>
2016-02-05 17:20:34 +0100

    Fächerverwaltung erstellt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,7 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ url('/fach') }}">F&auml;cher</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
