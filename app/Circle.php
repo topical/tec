a1ab7723eff858933377c9df471f7af2203f67ba
Celine Suchanek <celine.suchanek1611@gmail.com>
2016-02-27 13:06:07 +0100

    Relation für Circle und Batch eingetragen

--- app/Circle.php
+++ app/Circle.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Circle extends Model
 {
     protected $table = 'circle';
+    
+    public function subject()
+    {
+    	return $this->belongsTo('App\Subject');
+    }
+    
+    public function registration()
+    {
+    	return $this->hasMany('App\Registration');
+    }
+    
+    public function batch()
+    {
+    	return $this->hasMany('App\Batch');
+    }
+    
+    public function pupil()
+    {
+    	return $this->belongsToMany('App\Pupil', 'registration');
+    }
+    
+    public function submission()
+    {
+    	return $this->hasManyThrough('App\Submission', 'batch');
+    }
 }
--- app/Submission.php
+++ app/Submission.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Submission extends Model
 {
 	protected $table = 'submission';
+	
+	public function pupil()
+	{
+		return $this->belongsTo('App\Pupil');
+	}
+	
+	public function batch()
+	{
+		return $this->belongsTo('App\Batch');
+	}
+	
 }
