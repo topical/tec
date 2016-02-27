f15a6e788544d174536e1bc81b81544ef1045005
Elea1234 <email@elea.cc>
2016-02-27 13:03:43 +0100

    Relationen von Schüler und Registration

--- app/Pupil.php
+++ app/Pupil.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Pupil extends Model
 {
     protected $table = 'pupil';
+    
+    public function school()
+    {
+    	return $this->belongsTo('App\School');
+    }
+    
+    public function circle()
+    {
+    	return $this->belongsToMany('App\Circle', 'registration');
+    }
+    
+    public function registration()
+    {
+    	return $this->hasMany('App\Registration');
+    }
+    
+    public function submission()
+    {
+    	return $this->hasMany('App\Submission');
+    }
+    
+    public function batch()
+    {
+    	return $this->belongsToMany('App\Batch', 'submission');
+    }  
 }
--- app/Registration.php
+++ app/Registration.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Registration extends Model
 {
     protected $table = 'registration';
+    
+    public function pupil()
+    {
+    	return $this->belongsTo('App\Pupil');
+    }
+    
+    public function circle()
+    {
+    	return $this->belongsTo('App\Circle');
+    }
+    
 }
