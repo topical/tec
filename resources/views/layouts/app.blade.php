b6845ed9da2833493d955b000527dac92d35ee96
Elea1234 <email@elea.cc>
2016-03-21 21:53:28 +0100

    Texte übersetzt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Abmelden</a></li>
                         </ul>
                     </li>
                  </ul>
