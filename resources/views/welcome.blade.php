c2f763a11554b8aea04f709ad1451d2a46d96142
Elea1234 <email@elea.cc>
2016-03-21 22:29:22 +0100

    Startseite aufgehübscht.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,36 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Herzlich Willkommen</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
+                    <p>
+                    Mit dieser Webseite k&ouml;nnen Sie:
+					<ul>
+                    <li>F&auml;cher anlegen</li>
+                    <li>Schulen anlegen</li>
+                    <li>Sch&uuml;ler zuordnen</li>
+					<li>Korrespondenzzirkel anlegen</li>
+					<li>Teilnehmer zuweisen</li>
+					<li>Korrespondenzen anlegen</li>
+					<li>Punkte der einzelnen Teilnehmer eintragen</li>
+					</ul> 
+					<p>
+					Sie k&ouml;nnen jederzeit eine Auswertung der bisher erzielten Punkte erstellen.
+					</p>
+					<p>
+					Zu Beginn eines neuen Schuljahres m&uuml;ssen Sie zuerst rechts oben das Jahr wechseln.
+					</p>
+                    @can('is-admin')
+                    <p>
+                    Als Administrator k&ouml;nnen Sie zudem die Nutzer dieser Webseite verwalten.
+                    </p>
+					@endcan
+					<br/>
+					<p>
+					<b>Viel Spass!</b>
+					</p>
+				</div>
             </div>
         </div>
     </div>
